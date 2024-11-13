479aaca1ad195f1ba4086a85323adbb7df2f84c4
Heath Croasdale <thebeast126@hotmail.com>
2024-11-13 14:07:54 +0000

    add excerpt to Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-heath1979-djangoblog-ql9z1un5adq.ws.codeinstitute-ide.net', '.herokuapp.com']
 
