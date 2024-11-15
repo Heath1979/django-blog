aea00d1acab8fff4120ace6386c4840f2c5a9021
Heath Croasdale <thebeast126@hotmail.com>
2024-11-15 13:29:24 +0000

    add tests

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-heath1979-djangoblog-ql9z1un5adq.ws.codeinstitute-ide.net', '.herokuapp.com']
                   
